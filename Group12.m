ddedd99c11886242ec009cb20d16bf89891f6576
whatamidoinglmao <76539111+whatamidoinglmao@users.noreply.github.com>
2022-10-08 19:57:23 -0700

    Created deck.

--- Group12.m
+++ Group12.m
@@ -10,8 +10,18 @@
 % 
 %=========================================================================
 
-clear all
+% define face cards
+ace = 11;
+jack = 10;
+queen = 10;
+king = 10;
 
-disp("hello")
+% create "deck" as array with 52 elements
+deck = [ace,ace,ace,ace,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6 ... 
+        7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,jack,jack,jack,jack, ...
+        queen,queen,queen,queen,king,king,king,king];
 
-disp("bye")
+% shuffles the deck
+shuffle = randperm(52);
+
+
