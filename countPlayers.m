54ed7fd5c716f55229e5e89473ef35da5bdbe11e
whatamidoinglmao <iamasweetpotato@gmail.com>
2022-10-10 17:22:13 -0700

    Removed clear and clc

--- countPlayers.m
+++ countPlayers.m
@@ -1,8 +1,4 @@
-clear
-clc
-
 playerNumber = input("how many players? ");
-
 
 player_index = createPlayers(playerNumber);
 
@@ -12,9 +8,8 @@ for j = 1:size(player_index,1)
 end
 
 clear j
-clear player_index
 
-function [players] = createPlayers(amount)
+function players = createPlayers(amount)
     for i = 1:amount
         a = (1:amount)';
         matrix = zeros(1,2);
