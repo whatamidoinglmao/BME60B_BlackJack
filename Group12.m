dcdbbc2cd4ab0466e1ba2d6debfaef1ea4380929
whatamidoinglmao <iamasweetpotato@gmail.com>
2022-10-18 09:44:22 -0700

    Finalized document. Should be done now

--- Group12.m
+++ Group12.m
@@ -2,13 +2,19 @@
 % 
 %  BME 60B, Fangyuan Ding, MWF 4:00 - 4:50pm
 %  
+%  Problem Set 1 (Blackjack)
+%
 %  Group 12:
-%  Elian L, 
+%  Kentstar Samuel Harsono, 
+%  Eric Hyun Kim, 
+%  Tair Kuzhekov, 
+%  Elian Martin Sian Lintag
 %  
 %  Last Update:
-%  [DATE]
+%  10/18/2022
 % 
 %==========================================================================
+
 % inital clear
 clear
 clc
